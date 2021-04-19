6f2cef3eea90db57def6962b3498b4b95a08ac72
Aihab Umair <addisonhardley111@gmail.com>
2021-04-19 22:39:21 +0530

    Added navbar and responsivity for tablets

--- src/components/common/Navbar/navbar.js
+++ src/components/common/Navbar/navbar.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import Logo from '../Logo/logo'
 import * as classes from './navbar.module.css'
 
-const navbar = () => {
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    const navbarToggleHandler = () => {
+        setNavbarOpen(!navbarOpen)
+    }
+
+    const navbarCloseHandler = () => {
+        setNavbarOpen(false)
+    }
+
     return (
         <div className={classes.navbar}>
             <Link to="/">
                 <Logo />
             </Link>
-            <div className={classes.links}>
-                <div><Link to="/#about">ABOUT</Link></div>
-                <div><Link to="/#featuredProjects">PROJECTS</Link></div>
-                <div><Link to="/#contact">CONTACT</Link></div>
-                <div>RESUME</div>
+            <div className={classes.links + ' ' + (navbarOpen && ' ' + classes.open)}>
+                <div><Link to="/#about" onClick={navbarCloseHandler}>ABOUT</Link></div>
+                <div><Link to="/#featuredProjects" onClick={navbarCloseHandler}>PROJECTS</Link></div>
+                <div><Link to="/#contact" onClick={navbarCloseHandler}>CONTACT</Link></div>
+                <div onClick={navbarCloseHandler}>RESUME</div>
+            </div>
+            <div className={classes.hamburger + ' ' + (navbarOpen && ' ' + classes.open)}>
+                <button onClick={navbarToggleHandler}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
             </div>
         </div>
     )
 }
 
-export default navbar
+export default Navbar
